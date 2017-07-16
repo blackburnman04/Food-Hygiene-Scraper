d3369d8baf2a66f1cd7d143daefe9c6ecdb2e353
blackburnman04 <brendan.rodgers@gmail.com>
2017-07-16 13:43:52 +0100

    Update RestaurantScraper.py

--- RestaurantScraper.py
+++ RestaurantScraper.py
@@ -1,87 +1,99 @@
 import requests
 import time
+import csv
 import sys
 from bs4 import BeautifulSoup
 
+hygiene = []
 
-class RestaurantScraper(object):
+def deletelist():
+    hygiene.clear()
 
-    def __init__(self, pc):
-        self.pc = pc        # the input postcode
-        self.max_page = self.find_max_page()        # The number of page available
-        self.restaurants = list()       # the final list of restaurants where the scrape data will at the end of process
 
-    def run(self):
-        for url in self.generate_pages_to_scrape():
-            restaurants_from_url = self.scrape_page(url)
-            self.restaurants += restaurants_from_url     # we increment the  restaurants to the global restaurants list
+def savefile():
+    filename = input("Please input name of file to be saved")        
+    with open (filename + '.csv','w') as file:
+       writer=csv.writer(file)
+       writer.writerow(['Address','Town', 'Price', 'Period'])
+       for row in hygiene:
+          writer.writerow(row)
+    print("File Saved Successfully")
 
-    def create_url(self):
-        """
-        Create a core url to scrape
-        :return: A url without pagination (= page 1)
-        """
-        return "https://www.scoresonthedoors.org.uk/search.php?name=&address=&postcode=" + self.pc + \
-               "&distance=1&search.x=8&search.y=6&gbt_id=0&award_score=&award_range=gt"
 
-    def create_paginated_url(self, page_number):
-        """
-        Create a paginated url
-        :param page_number: pagination (integer)
-        :return: A url paginated
-        """
-        return self.create_url() + "&page={}".format(str(page_number))
+def appendhygiene(scrape):
+    hygiene.append(scrape)
 
-    def find_max_page(self):
-        """
-        Function to find the number of pages for a specific search.
-        :return: The number of pages (integer)
-        """
-        time.sleep(5)
-        r = requests.get(self.create_url())
-        soup = BeautifulSoup(r.content, "lxml")
-        pagination_soup = soup.findAll("div", {"id": "paginator"})
-        pagination = pagination_soup[0]
-        page_text = pagination("p")[0].text
-        return int(page_text.replace('Page 1 of ', ''))
+def makesoup(url):
+    page=requests.get(url)
+    print(url + "  scraped successfully")
+    return BeautifulSoup(page.text,"lxml")
 
-    def generate_pages_to_scrape(self):
-        """
-        Generate all the paginated url using the max_page attribute previously scraped.
-        :return: List of urls
-        """
-        return [self.create_paginated_url(page_number) for page_number in range(1, self.max_page + 1)]
+def hygienescrape(g_data):
+    for item in g_data:
+        try:
+            name = (item.find_all("a", {"class": "name"})[0].text)
+        except:
+            pass
+        try:
+            address = (item.find_all("span", {"class": "address"})[0].text)
+        except:
+            pass
+        try:
+            bleh = item.find_all('img', {'alt': True})[0]['alt']
+            appendhygiene(scrape=[name,address,bleh])
+        except:
+            pass
 
-    def scrape_page(self, url):
-        """
+
+def hygieneratingsbyrating():
+    
+    search = input("Please enter postcode")
+    rating = input("Please enter hygiene rating")
+    soup=makesoup(url = "https://www.scoresonthedoors.org.uk/search.php?name=&address=" + search + "&postcode=&distance=1&search.x=16&search.y=21&gbt_id=0&award_score=fhrs" + rating)
+    hygienescrape(g_data = soup.findAll("div", {"class": "search-result"}))
+    
+    button_next = soup.find("a", {"rel": "next"}, href=True)
+    while button_next:
+        time.sleep(2)#delay time requests are sent so we don't get kicked by server
+        soup=makesoup(url = "https://www.scoresonthedoors.org.uk/search.php{0}".format(button_next["href"]))
+        hygienescrape(g_data = soup.findAll("div", {"class": "search-result"}))
+
+        button_next = soup.find("a", {"rel" : "next"}, href=True)
         
-        :return:
-        """
-        time.sleep(5)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, "lxml")
-        g_data = soup.findAll("div", {"class": "search-result"})
-        ratings = soup.select('div.rating-image img[alt]')
-        restaurants = list()
-        for item in g_data:
-            name = print (item.find_all("a", {"class": "name"})[0].text)
-            restaurants.append(name)
-            try:
-                print (item.find_all("span", {"class": "address"})[0].text)
-            except:
-                pass
-            try:
-                for rating in ratings:
-                    print (rating ['alt'])[0].text
-            except:
-                pass
-        return restaurants
+        
+        
+def hygieneratings():
+    
+    search = input("Please enter postcode")
+    soup=makesoup(url = "https://www.scoresonthedoors.org.uk/search.php?name=&address=&postcode=" + search + "&distance=1&search.x=16&search.y=21&gbt_id=0")
+    hygienescrape(g_data = soup.findAll("div", {"class": "search-result"}))
+    
+    button_next = soup.find("a", {"rel": "next"}, href=True)
+    while button_next:
+        time.sleep(2)#delay time requests are sent so we don't get kicked by server
+        soup=makesoup(url = "https://www.scoresonthedoors.org.uk/search.php{0}".format(button_next["href"]))
+        hygienescrape(g_data = soup.findAll("div", {"class": "search-result"}))
+
+        button_next = soup.find("a", {"rel" : "next"}, href=True)
 
 
+def menu():
+        strs = ('Enter 1 to search Food Hygiene ratings by Postcode \n'
+                'Enter 2 to search by Post Code and Hygiene Rating Score\n'
+                'Enter 3 to Exit\n' )
+        choice = input(strs)
+        return int(choice) 
 
-if __name__ == '__main__':
-    pc = input('Give your post code')
-    scraper = RestaurantScraper(pc)
-    scraper.run()
-    print ("{} restaurants scraped".format(str(len(scraper.restaurants))))
+while True:          #use while True
+    choice = menu()
+    if choice == 1:
+        hygieneratings()
+        savefile()
+        deletelist()
+    elif choice == 2:
+        hygieneratingsbyrating()
+        savefile()
+        deletelist()
+    elif choice == 3:
+        break
 
